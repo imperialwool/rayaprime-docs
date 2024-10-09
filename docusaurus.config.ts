1a8377f82150d1c0cffa60c3596a9dbb87bf4ac9
imperialwool <hi@iwool.dev>
2024-10-09 03:05:16 +0200

    upd: больше команд объяснено

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -60,7 +60,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docs',
           position: 'left',
           label: 'Документация',
         },
